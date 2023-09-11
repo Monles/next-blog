4d91d1bbe73275d8a629045022d92942f3eedccb
Monles <iceueb@gmail.com>
2023-09-11 15:23:38 +0100

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,35 @@
-import styles from "./homepage.module.css";
+import styles from './homepage.module.css'
+import Link from 'next/link'
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Link href='/'>
+        <h1>
+          Reprehenderit Lorem labore cupidatat pariatur mollit. Officia ullamco
+          id duis quis. Ipsum anim voluptate veniam cillum incididunt minim sunt
+          cupidatat.
+        </h1>
+        Consequat tempor cillum aute ut velit laboris mollit sit exercitation et
+        commodo officia pariatur. Mollit occaecat ipsum in est sit consectetur
+        qui eu. Ut aute enim laboris anim non magna. Nulla eiusmod labore esse
+        do nisi duis proident occaecat ea ex veniam dolore. Ut officia dolore
+        excepteur commodo. Proident enim consectetur reprehenderit fugiat ad
+        magna est do incididunt. Sit adipisicing dolor velit pariatur. Pariatur
+        officia ad in fugiat eu ullamco proident esse nulla. Et anim ut ipsum
+        mollit enim mollit incididunt. Aliquip eiusmod do cupidatat quis fugiat
+        tempor dolore ad. Culpa excepteur irure fugiat qui magna sint Lorem
+        sint. Deserunt pariatur consectetur deserunt elit ipsum non velit elit
+        dolore et ut adipisicing. Aliqua sit et qui cillum eiusmod amet ut
+        nulla. Laboris quis cillum fugiat adipisicing sit duis. Elit duis minim
+        tempor velit cupidatat id tempor eu ex tempor. Nisi reprehenderit velit
+        nulla incididunt. Incididunt non culpa nostrud qui elit sint anim dolor
+        nisi Lorem. Eiusmod aute id tempor labore mollit adipisicing sint culpa.
+        Sunt eu mollit tempor consequat ullamco. Culpa eiusmod dolore fugiat
+        amet eiusmod. Nostrud enim et do id nulla enim labore Lorem dolor
+        proident in ullamco id incididunt. Irure ut ullamco officia laborum sunt
+        officia ad anim.
+      </Link>
+    </div>
+  )
 }
